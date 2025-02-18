2a4f18b76772e6c7ce1934333212c054b585cfd1
hrprtkaur88 <kaurharpreet@microsoft.com>
2025-02-18 12:37:26 -0500

    Removing DI from Index method

--- equipment-classification-agent-api/Services/AzureAISearchService.cs
+++ equipment-classification-agent-api/Services/AzureAISearchService.cs
@@ -13,8 +13,8 @@ namespace equipment_classification_agent_api.Services;
 
 public interface IAzureAISearchService
 {
-    Task<SearchIndex> CreateAISearchIndexAsync();
-    Task IndexDataAsync(SearchIndex searchIndex);
+    Task CreateAISearchIndexAsync();
+    Task IndexDataAsync();
 }
 
 public class AzureAISearchService : IAzureAISearchService
@@ -61,7 +61,7 @@ public class AzureAISearchService : IAzureAISearchService
         _azureSQLService = azureSQLService;
     }
 
-    public async Task<SearchIndex> CreateAISearchIndexAsync()
+    public async Task CreateAISearchIndexAsync()
     {
 
         SearchIndex searchIndex = new(_indexName)
@@ -144,10 +144,9 @@ public class AzureAISearchService : IAzureAISearchService
 
         _logger.LogInformation($"Completed creating index {searchIndex}");
 
-        return searchIndex;
     }
 
-    public async Task IndexDataAsync(SearchIndex searchIndex)
+    public async Task IndexDataAsync()
     {
         var stopwatch = Stopwatch.StartNew();
         var golfBalls = await _azureSQLService.GetGolfBallsAsync();
