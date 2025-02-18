e7c032eb2206aba8d913f52ceded085cad2e0a39
William Xifaras <wxifaras@outlook.com>
2025-02-18 14:36:38 -0500

    AzureStorageOptions

--- equipment-classification-agent-api/Models/AzureStorageOptions.cs
+++ equipment-classification-agent-api/Models/AzureStorageOptions.cs
@@ -1,10 +1,15 @@
-﻿namespace equipment_classification_agent_api.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace equipment_classification_agent_api.Models
 {
     public class AzureStorageOptions
     {
         public const string AzureStorage = "AzureStorageOptions";
 
+        [Required]
         public string StorageConnectionString { get; set; }
+
+        [Required]
         public string ImageContainer { get; set; }
     }
 }
