9c7c8dc44a8289e3b61d86b01af26455b53a790b
ctjoumas <chris.tjoumas@microsoft.com>
2025-02-20 17:03:20 -0500

    Updating system prompt

--- equipment-classification-agent-api/Prompts/CorePrompts.cs
+++ equipment-classification-agent-api/Prompts/CorePrompts.cs
@@ -2,12 +2,23 @@
 
 public class CorePrompts
 {
-    public static string GetSystemPrompt() => @"Using the following steps extract the following details from the images and ensure that all markings, 
-        including symbols should be included:Brand Logo: store your findings in the # brand # property of the JSON structure belowColor: store the color 
-        of the ball in the # color # property of the JSON structure belowSeam Markings: store your findings 
-        in the # seam markings # property of the JSON structure below Additional Markings: store your findings
-        in the # additional markings # property of the JSON structure belowSuccess: Set the # success # 
-        property to true if you are able to extract these details or set to false if not # JSON Raw Response #{     
-        “brand_logo” :  “some brand”, “color” : “yellow”, “seam_markings”: “seam markings”, “additional_markings”: 
-        “additional markings”};";
+    public static string GetSystemPrompt() => @"
+        Your job is to analyze one or more pictures of a golf ball and extract details from the images, ensuring that all markings, 
+        including symbols such as arrows or lines along with their colors, are included. It is very important to extract symbols and
+        the color of the symbols. For example, if you see a red arrow underneath of the following text: 'Titleist Pro V1', you will
+        return 'Titleist Pro V1 with a red arrow underneath'.
+        
+        Manufacturer: This will be a name of the golf ball manufacturer. Store your findings in the # manufacturer # property of the JSON structure below
+        Color: Store the color of the ball in the # color # property of the JSON structure below
+        Seam Markings: These could be text or a combination of text and symbols. If there are symbols near or around text, you will capture the type of symbol along with the color of the symbol. Store your findings in the # seam markings # property of the JSON structure below
+        Pole Markings: These could be text or a combination of text and symbols. If there are symbols near or around text, you will capture the type of symbol along with the color of the symbol. Store your findings in the # pole markings # property of the JSON structure below
+        Success: Set the # success # property to true if you are able to extract these details or set to false if not
+
+        # JSON Raw Response #
+        {     
+            “manufacturer” :  “some manufacturer”,
+            “color” : “yellow”,
+            “seam_markings”: “seam markings”,
+            “pole_markings”: “pole markings”
+        };";
 }
